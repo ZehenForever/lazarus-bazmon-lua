7cb50590e09924367c2c5c9d3848b2dcdd5e9ed1
ZehenForever <zehen@triplenexus.org>
2024-03-18 14:09:31 -0600

    Frontend now with documentation, and can present a UI for the search interface

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # lazarus-bazmon-lua
 A Bazaar search tool Lua script for the Lazarus EQ Emulator server
+
+## Motivation
+The EQ Bazaar interface does not work on Lazarus, so people use the excellent hosted Magelo web site (https://www.lazaruseq.com/Magelo/index.php?page=bazaar) for the Lazarus server.
+
+We are now able to query the Bazaar in game.
+
+## How it works
+This is achieved by running two scripts:
+
+1. **Front end**: A normal Lua script ([found here](https://github.com/ZehenForever/lazarus-bazmon-lua)) that is run by MQ's `/lua run <script>` functionality.  It presents the UI and writes its data to a standard .ini file just like any other plugin.  It reads query result data from a separate CSV file populated by the backend server.
+1. **Back end**: A standard Go program ([found here](https://github.com/ZehenForever/lazarus-bazmon-server)) that watches for changes in the Lua .ini file and uses that to essentially proxy queries to the Lazarus Magelo Bazaar search page.  It writes query results to a CSV file that can be in turn read by the Lua script to show those search results in game.
+
+## Usage
+
+### Set up the front end (this)
+
+Either download the zip from Github and unpack it alongside your other Lua scripts, or clone the repo to your Lua scripts directory:
+
+```
+cd C:\PathToE3MQ\lua
+git clone git@github.com:ZehenForever/lazarus-bazmon-lua.git bazmon
+```
+
+Run the Lua script in game:
+```
+/lua run bazmon
+```
+
+This will launch the UI which is pretty self explanatory.
+
+The basic flow is:
+1. When you click the "Search" button, it will record that query request into its `BazMon.ini` ini file.
+1. If the backend server is running, it will see this file change, will parse out the query from the .ini file, and run a query agains the Lazarus Magelo Bazaar page.
+1. The backend server will then parse those Bazaar search results and write them to the `BazResults.csv` file.
+1. The front end will poll that file until a configurable timeout (currently 5 seconds), and once there are matching results in the CSV, it will paint those in game in the UI as a table of search results.
+
+### Set up the backend
+Setting up and using the the backend Go program can be found in [its repository](https://www.lazaruseq.com/Magelo/index.php?page=bazaar).
--- init.lua
+++ init.lua
@@ -8,9 +8,9 @@ local Write = require('lib/Write')
 local ini = require('lib/inifile')
 local util = require('lib/util')
 local ftcsv = require('lib/ftcsv')
+local imgui = require('ImGui')
 
--- Time in ms to wait for things like windows opening
-local WaitTime = 750
+local open_gui = true
 
 -- Time in seconds to wait for the bazaar to respond
 local bazaar_wait = 5
@@ -38,6 +38,12 @@ local save_settings = function (table)
     ini.save(config_file, table)
 end
 
+-- convenience function to trim whitespace from a string
+local trim_space = function(s)
+    --return (s:gsub("^%s*(.-)%s*$", "%1"))
+    return s:match( "^%s*(.-)%s*$" )
+end
+
 -- Settings originally set to the example
 local settings = example_config
 
@@ -48,10 +54,21 @@ local parse_item = function(item)
     return value, comparator
 end
 
+local current_query_id = "uu5r0zq6zbz4kkv5"
+local start_search = false
+local start_save_settings = false
+local currently_searching = false
+local search_results = {}
+
 -- Searches the CSV results file for a queryID
 -- Waits for a configured number of seconds for the results to be written to the file
-local search_results = function(queryID)
+local load_search_results = function(queryID)
     Write.Debug('Reading CSV file: %s', results_file)
+
+    currently_searching = true
+
+    -- Reset the search results
+    search_results = {}
 
     -- loop until time is up, waiting for the results to be written to the file
     local found_item = false
@@ -62,13 +79,106 @@ local search_results = function(queryID)
             if result.QueryID == queryID then
                 found_item = true
                 Write.Debug('Result[%d]: %s "%s" %s %s', index, result.QueryID, result.Item, result.Price, result.Seller)
+                search_results[#search_results+1] = result
             end
         end
         if found_item then
             break
         end
     end
-    
+
+    currently_searching = false
+end
+
+local search_item_name = ""
+
+local render_ui = function(open)
+    local main_viewport = imgui.GetMainViewport()
+    imgui.SetNextWindowPos(main_viewport.WorkPos.x + 650, main_viewport.WorkPos.y + 20, ImGuiCond.FirstUseEver)
+
+    -- change the window size
+    imgui.SetNextWindowSize(600, 300, ImGuiCond.FirstUseEver)
+
+    local open, show = imgui.Begin("BazMonitor", true)
+
+    if not show then
+        ImGui.End()
+        return open
+    end
+
+    ImGui.PushItemWidth(ImGui.GetFontSize() * -12)
+
+    -- Beginning of window elements
+    imgui.Text("Bazaar Search")
+
+    -- Item name search input box
+    search_item_name, _ = imgui.InputText("Item Name", search_item_name, ImGuiInputTextFlags.EnterReturnsTrue)
+
+    -- Search button
+    if ImGui.Button("Search") then
+        -- What you want the button to do
+        search_item_name = trim_space(search_item_name)
+        Write.Debug('Searching for "%s"', search_item_name)
+
+        -- Add the search to our INI file
+        local query_id = util.random_string(16)
+
+        -- Clear the queries, keep it a single query for now
+        settings['Queries'] = {} 
+        settings['Queries'][query_id] = string.format("/name|%s", search_item_name)
+
+        -- Set the current query ID to the one we just created
+        current_query_id = query_id
+
+        -- Signal that we want to save the settings to the INI file
+        -- Saving our INI file will trigger the external script to query the bazaar
+        start_save_settings = true
+
+        -- Signal that we want to start searching for the results
+        -- This will poll the results file for the queryID
+        start_search = true
+    end
+    ImGui.Separator()
+
+    if currently_searching then
+        imgui.Text("Searching for %s ...", search_item_name)
+        ImGui.Separator()
+    else
+        imgui.Text("Search Results")
+
+        -- Results table
+        if ImGui.BeginTable('BazResults', 4, ImGuiTableFlags.Borders) then
+            ImGui.TableSetupColumn('QueryID', ImGuiTableColumnFlags.DefaultSort)
+            ImGui.TableSetupColumn('Item', ImGuiTableColumnFlags.DefaultSort)
+            ImGui.TableSetupColumn('Price', ImGuiTableColumnFlags.DefaultSort)
+            ImGui.TableSetupColumn('Seller', ImGuiTableColumnFlags.DefaultSort)
+            ImGui.TableHeadersRow()
+
+            for i, result in ipairs(search_results) do
+                ImGui.TableNextRow()
+                ImGui.TableNextColumn()
+                ImGui.Text(result.QueryID)
+                ImGui.TableNextColumn()
+                ImGui.Text(result.Item)
+                ImGui.TableNextColumn()
+                ImGui.Text(result.Price)
+                ImGui.TableNextColumn()
+                ImGui.Text(result.Seller)
+            end
+            ImGui.EndTable()
+        end
+
+    end
+
+
+    -- End of main window element area --
+
+    -- Required for window elements
+    imgui.Spacing()
+    imgui.PopItemWidth()
+    imgui.End()
+
+    return open
 end
 
 local bazmonitor = function(...)
@@ -138,6 +248,11 @@ local bazmonitor = function(...)
         end
     end
 
+    if args[1] == 'ui' then
+        Write.Debug('Rendering UI')
+        render_ui()
+    end
+
 end
 
 -- Load the settings from the config file
@@ -156,8 +271,20 @@ mq.bind('/bazmon', bazmonitor)
 
 Write.Debug('Using BazMonitor config file: %s', config_file)
 
+ImGui.Register('rend_ui', function()
+    open_gui = render_ui(open_gui)
+end)
+
 -- Loop and yield on every frame
-while true do
+while open_gui do
     mq.doevents()
+    if start_save_settings then
+        start_save_settings = false
+        save_settings(settings)
+    end
+    if start_search then
+        start_search = false
+        load_search_results(current_query_id)
+    end
     mq.delay(1)
 end
